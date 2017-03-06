12793740d2801977ba48c71863761cedb3f47aed
naeemb123 <naeemb_2k7@hotmail.co.uk>
2017-03-06 19:44:31 +0000

    BLE moved to nearby intent

--- app/src/main/java/hci/attired/NearBy.java
+++ app/src/main/java/hci/attired/NearBy.java
@@ -1,5 +1,15 @@
 package hci.attired;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -7,9 +17,11 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -38,6 +50,15 @@ public class NearBy extends AppCompatActivity {
 
     private String item;
 
+    //ble
+    private BluetoothAdapter bleDev = null;
+    private BluetoothLeScanner scanner = null;
+    // request ID for enabling Bluetooth
+    private static final int REQUEST_ENABLE_BT = 1000;
+
+    private boolean isScanning = false;
+    private int scanMode = ScanSettings.SCAN_MODE_BALANCED;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,6 +72,44 @@ public class NearBy extends AppCompatActivity {
         // finally change the color
         window.setStatusBarColor(ContextCompat.getColor(this, R.color.statsbar));
         setContentView(R.layout.activity_near_by);
+
+
+        //=========================================================BLE======================================
+        // retrieve the BluetoothManager instance and check if Bluetooth is enabled. If not the
+        // user will be prompted to enable it and the response will be checked in onActivityResult
+        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        bleDev = bluetoothManager.getAdapter();
+        if (bleDev == null || !bleDev.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        if(scanner == null) {
+            scanner = bleDev.getBluetoothLeScanner();
+            if(scanner == null) {
+                // probably tried to start a scan without granting Bluetooth permission
+                Toast.makeText(this, "Failed to start scan (BT permission granted?)", Toast.LENGTH_LONG).show();
+                Log.w(TAG, "Failed to get BLE scanner instance");
+                return;
+            }
+        }
+
+        Toast.makeText(this, "Starting BLE scan...", Toast.LENGTH_SHORT).show();
+
+        // clear old scan results
+//        scanAdapter.clear();
+
+        List<ScanFilter> filters = new ArrayList<>();
+        ScanSettings settings = new ScanSettings.Builder().setScanMode(scanMode).build();
+        scanner.startScan(filters, settings, bleScanCallback);
+        isScanning = true;
+
+
+
+
+
+
+
 
         SharedPreferences prefs = getSharedPreferences(ATTIRE_STORAGE, MODE_PRIVATE);
         item = prefs.getString("item1", "");
@@ -84,6 +143,137 @@ public class NearBy extends AppCompatActivity {
         });
 
     }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // stop any in-progress scan and stop updating the graph if activity is paused
+        stopScan();
+    }
+
+    private void stopScan() {
+        if(scanner != null && isScanning) {
+            Toast.makeText(this, "Stopping BLE scan...", Toast.LENGTH_SHORT).show();
+            isScanning = false;
+            Log.i(TAG, "Scan stopped");
+            scanner.stopScan(bleScanCallback);
+        }
+    }
+
+
+
+
+    // class implementing BleScanner callbacks
+    private ScanCallback bleScanCallback = new ScanCallback() {
+
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            super.onScanResult(callbackType, result);
+
+            final BluetoothDevice dev = result.getDevice();
+            final int rssi = result.getRssi();
+
+            Log.d(TAG,dev.getAddress());
+            if(dev != null && isScanning) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // retrieve device info and add to or update existing set of beacon data
+                        String name = dev.getName();
+                        String address = dev.getAddress();
+                        Log.d(TAG,"Name of the BLE!!!!!! --> "+ name);
+                        Log.d(TAG,"Address of the BLE!!!!!! --> "+ address);
+//                        scanAdapter.update(dev, address, name == null ? address.toString(): name, rssi);
+                    }
+
+                });
+            }
+        }
+
+        @Override
+        public void onBatchScanResults(List<ScanResult> results) {
+            super.onBatchScanResults(results);
+            Log.d(TAG, "BatchScanResult(" + results.size() + " results)");
+        }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+            super.onScanFailed(errorCode);
+            Log.w(TAG, "ScanFailed(" + errorCode + ")");
+        }
+    };
+
+
+    private class BeaconInfo {
+        public BluetoothDevice device;
+        public String address;
+        public String name;
+        public int rssi;
+
+        private static final int WINDOW_SIZE = 9;
+        private int[] window = new int[WINDOW_SIZE];
+        private int windowptr = 0;
+
+        public BeaconInfo(BluetoothDevice device, String address, String name, int rssi) {
+            this.device = device;
+            this.address = address;
+            this.name = name;
+            this.rssi = rssi;
+            for(int i=0;i<WINDOW_SIZE;i++)
+                this.window[i] = rssi;
+        }
+
+        // called when a new scan result for this beacon is parsed
+        public void updateRssi(int newRssi) {
+            this.rssi = newRssi;
+            window[windowptr] = newRssi;
+            windowptr = (windowptr + 1) % WINDOW_SIZE;
+        }
+
+        // returns the latest raw RSSI reading for this beacon
+        public double getRssi() {
+            return this.rssi;
+        }
+
+        // returns a very simple moving average of the last WINDOW_SIZE
+        // RSSI values received for this beacon
+        public double getFilteredRssi() {
+            double mean = 0.0;
+            for(int i=0;i<WINDOW_SIZE;i++) {
+                mean += window[i];
+            }
+            mean /= WINDOW_SIZE;
+            return mean;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            // test if beacon objects are equal using their addresses
+            if(o != null && o instanceof BeaconInfo) {
+                BeaconInfo other = (BeaconInfo) o;
+                if(other.address.equals(address))
+                    return true;
+            }
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            // as with equals() use addresses to test equality
+            return address.hashCode();
+        }
+    }
+
+
+
+
+
+
+
+
+
 
     private void parseXMLFile() throws XmlPullParserException, IOException {
 
